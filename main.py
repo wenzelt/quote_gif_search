2ce3ebcb0bb371fb0d9f0a78ee41fa77a02c20d4
Tom Wenzel <tom.wenzel@limehome.de>
2021-06-07 17:37:50 +0200

    code improvements

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+"""This module converts local video and srt to gif."""
+
+
 import argparse
 import os
+import sys
 from typing import Union, Dict
 
 import chardet
@@ -11,6 +15,10 @@ WIDTH = "480"
 
 
 def parse_args():
+    """
+
+    @return:
+    """
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--video_file", "-v", type=str, required=True)
     arg_parser.add_argument("--subtitle", "-s", type=str, required=True)
@@ -23,34 +31,57 @@ def parse_args():
 
 
 def detect_encoding(subtitle_path: str) -> Dict[str, Union[float, str]]:
+    """
+
+    @param subtitle_path:
+    @return:
+    """
     with open(subtitle_path, "rb") as file:
         msg = file.read()
         result = chardet.detect(msg)
         return result
 
 
-def burn_subtitles(args: argparse.Namespace, time_duration: str):
+def burn_subtitles(args: argparse.Namespace, gif_length: str):
+    """
+
+    @param args:
+    @param gif_length:
+    """
     os.system(
-        f"ffmpeg -i {output_path}sub_short.mp4  -t {time_duration} -y -filter_complex '[0:v] "
+        f"ffmpeg -i {OUTPUT_PATH}sub_short.mp4  -t {gif_length} -y -filter_complex '[0:v] "
         f"fps={FPS},scale=w={WIDTH}:h=-1,split [a][b];[a]"
-        f"palettegen [p];[b][p] paletteuse=new=1' {output_path}{args.output_file}"
+        f"palettegen [p];[b][p] paletteuse=new=1' {OUTPUT_PATH}{args.output_file}"
     )
 
 
-def search_for_subtitle(subs: SubRipFile, search_term: str) -> list:
-    matched_subs = []
+def search_for_subtitle(subs: SubRipFile, search_string: str) -> list:
+    """
+
+    @param subs:
+    @param search_string:
+    @return:
+    """
+    search_hits = []
     for data in subs.data:
-        if search_term in data.text_without_tags:
-            matched_subs.append(data)
-    if matched_subs:
-        return matched_subs
-    else:
-        print("Could not find in sub")
-        exit(1)
+        if search_string in data.text_without_tags:
+            search_hits.append(data)
+    if search_hits:
+        return search_hits
+
+    print(f"Could not find '{search_string}' in sub")
+    sys.exit(1)
 
 
-def cut_subs(matched_subs, subs_pysrt, start_time):
-    matched_sub = matched_subs[0]
+def cut_subs(matched_text, subs_pysrt, start_time):
+    """
+
+    @param matched_text:
+    @param subs_pysrt:
+    @param start_time:
+    @return:
+    """
+    matched_sub = matched_text[0]
     part = subs_pysrt.slice(
         starts_after={
             "minutes": int(matched_sub.start.minutes),
@@ -72,7 +103,7 @@ if __name__ == "__main__":
 
     search_term = cmd_args.find
     matched_subs = search_for_subtitle(py_srt, search_term)
-    subtitle_start_time = str(
+    SUBTITLE_START_TIME = str(
         int(
             matched_subs[0].start.hours * 3600
             + matched_subs[0].start.minutes * 60
@@ -81,23 +112,23 @@ if __name__ == "__main__":
         - 10
     )
 
-    py_srt = cut_subs(matched_subs, py_srt, subtitle_start_time)
+    py_srt = cut_subs(matched_subs, py_srt, SUBTITLE_START_TIME)
 
     py_srt.save("output/cut_subs.srt", encoding="utf-8")
     # subtitle_start_time = matched_subs[0].start
     print(f"where from:{cmd_args.video_file}")
     print(f"where from:{cmd_args.subtitle}")
 
-    time_duration = "15"  # duration of clip
-    output_path = "output/"
+    TIME_DURATION = "15"  # duration of clip
+    OUTPUT_PATH = "output/"
     encoding = detect_encoding(cmd_args.subtitle)
 
     os.system(
-        f'ffmpeg -ss {subtitle_start_time} -i {cmd_args.video_file} '
-        f'-vf subtitles=output/cut_subs.srt '
-        f' -t {time_duration} -y  {output_path}/sub_short.mp4'
+        f"ffmpeg -ss {SUBTITLE_START_TIME} -i {cmd_args.video_file} "
+        f"-vf subtitles=output/cut_subs.srt "
+        f" -t {TIME_DURATION} -y  {OUTPUT_PATH}/sub_short.mp4"
     )
 
-    burn_subtitles(cmd_args, time_duration)
+    burn_subtitles(cmd_args, TIME_DURATION)
 
     print(f"Output Location:{cmd_args.output_file}")
